1ff905b8935e1e63909221e9a36b11480cbe95ab
Андрей Овчаренко <andrey.ovcharenko@branderstudio.com>
2022-12-08 23:15:03 +0200

    next

--- src/store.js
+++ src/store.js
@@ -1,4 +1,7 @@
 import * as redux from 'redux';
 import counter from './reducers';
 
-export default redux.createStore(counter);
+
+const store = redux.createStore(counter);
+store.state = store.getState();
+export default store;
